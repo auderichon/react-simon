0e0015709011dd28881cd6a9f8357957fd5d56aa
Aude Richon <auderichon@gmail.com>
2024-10-09 14:10:18 +0200

    🍱 update Routing component

--- src/core/routing.tsx
+++ src/core/routing.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../home";
 import { GamePage } from "../game";
 
 export const Routing: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/game" element={<GamePage />} />
-        {/* <Route path="*" element={<NotFoundPage />} /> */}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/game" element={<GamePage />} />
+      {/* <Route path="*" element={<NotFoundPage />} /> */}
+    </Routes>
   );
 };
